331849847c0e13ad35bcd827563aa348931d0ae6
Victor Glebov <victorglebovv@yandex.ru>
2023-09-05 18:35:47 +0500

    add: user router, model, dto, service

--- database.py
+++ database.py
@@ -2,5 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLACLHEMY_URL = "postgresql+psycopg2://root:admin@localhost:5432/flirtyfever"
+SQLACLHEMY_URL = "postgresql+psycopg2://postgres:admin@localhost:5432/flirtyfever"
 
+engine = create_engine(
+    SQLACLHEMY_URL
+    )
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal
+    try:
+        yield db
+    finally:
+        db.close() # Does not work properly - no close method
---
+++ routers/user.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from database import get_db
+
+from services import user as UserService
+from dto import user as UserDTO
+
+router = APIRouter()
+
+@router.post('/', tags=["user"])
+async def create_user(data: UserDTO.User = None, db: Session = Depends(get_db)):
+    return UserService.create_user(data, db)
+
+@router.get('/{id}', tags=["user"])
+async def get_user(id: int = None, db: Session = Depends(get_db)):
+    return UserService.get_user(id, db)
+
+@router.put('/{id}', tags=["user"])
+async def update_user(data: UserDTO.User = None, db: Session = Depends(get_db), id: int = None):
+    return UserService.update_user(data, db, id)
+
+@router.delete('/', tags=["user"])
+async def remove_user(id: int = None, db: Session = Depends(get_db)):
+    return UserService.remove_user(id, db)
