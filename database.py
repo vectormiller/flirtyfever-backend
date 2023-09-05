12ba62021e912f804273d34db94c1cdc97298d11
Victor Glebov <victorglebovv@yandex.ru>
2023-09-05 17:03:52 +0500

    add: entry point

---
+++ database.py
@@ -0,0 +1,6 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLACLHEMY_URL = "postgresql+psycopg2://root:admin@localhost:5432/flirtyfever"
+
