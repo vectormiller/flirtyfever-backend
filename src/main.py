0dfc97b7813638f46dd12f74fa4b8ae21eeeba5d
Victor Glebov <victorglebovv@yandex.ru>
2023-09-11 22:03:22 +0500

    add: pylint
    Code rating: 4.79/10
    
    Co-Authored-By: Alex <26963422+CxmmitSxicideInsxde@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -1,3 +1,25 @@
+"""
+    Main Module - FastAPI Application Entry Point
+
+    This module sets up the FastAPI app, includes routers, and runs the application using Uvicorn.
+
+    Modules and Packages:
+        - uvicorn: ASGI server used for running the FastAPI application.
+        - FastAPI: Web framework used to create RESTful APIs.
+        - database: Module for managing the database connection and ORM.
+        - routers.user: Router module for user-related API endpoints.
+
+    Usage:
+        To start the FastAPI application, run this module as the main script. It will launch the web server
+        on the specified host and port.
+
+    Example:
+        python main.py
+
+    Configuration:
+        You can configure the host, port, and other settings by modifying the `uvicorn.run` call at the end of this script.
+"""
+
 import uvicorn
 from fastapi import FastAPI
 
--- src/settings.py
+++ src/settings.py
@@ -1,12 +1,44 @@
+"""
+    Settings Module
+
+    This module is responsible for managing application settings and environment variables using Pydantic.
+
+    Modules and Packages:
+        - os: Provides access to operating system functionalities.
+        - pydantic_settings: BaseSettings class for creating Pydantic-based settings models.
+        - pydantic: Provides data validation and parsing using Python data types.
+        - dotenv: Loads environment variables from a .env file.
+        - exceptions: Custom exception for handling environment variable loading errors.
+
+    Classes:
+        - Settings: Pydantic model for defining application settings, such as database URL and root URL.
+
+    Functions:
+        - load_settings: Loads settings from environment variables into the Settings Pydantic model.
+
+    Usage:
+        1. Import the `settings` object to access application settings.
+            Ex: from settings import settings
+        2. Get needed environment variable.
+            Ex: SQLACLHEMY_URL = str(settings.DB_URL)
+
+    Configuration:
+        - See .env_example  file to discover which variables should be in the environment.
+        - Use a .env file to define environment variables for your application settings.
+"""
+
+from os import getenv
 from pydantic_settings import BaseSettings
 from pydantic import PostgresDsn
-from dotenv import load_dotenv, dotenv_values
-from os import getenv
+from dotenv import load_dotenv, dotenv_values # pylint: disable=unused-import
 from exceptions import EnvVarLoadingException
 
 load_dotenv()
 
 class Settings(BaseSettings):
+    """
+        Project Environment Variables
+    """
     DB_URL: PostgresDsn
     ROOT_URL: str
 
@@ -21,7 +53,7 @@ def load_settings() -> Settings:
     Settings - pydantic model with app settings
     """
     creds = {}
-    for field_name in Settings.model_fields.keys():
+    for field_name in Settings.model_fields:
         creds[field_name] = getenv(field_name)
 
     return Settings(**creds)
@@ -29,4 +61,4 @@ def load_settings() -> Settings:
 try:
     settings = load_settings()
 except Exception as e:
-    raise EnvVarLoadingException(e)
+    raise EnvVarLoadingException(e) from e
