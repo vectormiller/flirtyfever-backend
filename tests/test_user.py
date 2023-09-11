38a191857e65ad19a7b883ee50c5181696f9d0c1
Victor Glebov <victorglebovv@yandex.ru>
2023-09-11 19:06:19 +0500

    add: pytest config

--- tests/test_user.py
+++ tests/test_user.py
@@ -5,7 +5,7 @@ from src.settings import settings
 
 def test_getResponse():
     client = TestClient(app)
-    response = client.get(f"{settings.MAIN_URL}user/1")
+    response = client.get(f"{settings.ROOT_URL}user/1")
     assert response.status_code == 200
     # If you want to compare response body, use this:
     # assert response.json() == {"id": "1", "lastname": "Miller"}
