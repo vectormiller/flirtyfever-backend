3a53b15cd783e33149906d9a54c2e6b0e884b1aa
Victor Glebov <victorglebovv@yandex.ru>
2023-09-11 22:03:22 +0500

    add: pylint
    Code rating: 4.79/10
    
    Co-Authored-By: Alex <26963422+CxmmitSxicideInsxde@users.noreply.github.com>

--- src/database.py
+++ src/database.py
@@ -1,3 +1,12 @@
+"""
+    Database Utility Functions
+
+    This module provides utility functions for working with a database using SQLAlchemy.
+
+    Functions:
+        - get_db: Creates and manages a database connection session.
+"""
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from settings import settings
@@ -11,7 +20,13 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
-    db = SessionLocal()
+    """
+    Create and manage a database connection session.
+
+    Returns:
+        sqlalchemy.orm.Session: A SQLAlchemy session instance.
+    """
+    db = SessionLocal() # pylint: disable=invalid-name
     try:
         yield db
     finally:
--- src/exceptions.py
+++ src/exceptions.py
@@ -1,4 +1,27 @@
+"""
+    Exception types for custom exceptions.
+"""
+
 class ExceptionWithMessage(Exception):
+    """
+    Exception class that includes a custom message.
+    
+    This exception can be raised when you need to provide a specific error message along with the exception.
+
+    Attributes:
+        message (str): The custom error message associated with the exception.
+
+    Args:
+        message (str): The custom error message to be included in the exception.
+
+    Example:
+        try:
+            something()
+        except Exception:
+            raise ExceptionWithMessage("Unexpected behavior occurred, REF: #124")
+        
+        Output: ExceptionWithMessage: Unexpected behavior occurred, REF: #124
+    """
     def __init__(self, message: str, *args: object) -> None:
         super().__init__(*args)
         self.message = message
@@ -8,5 +31,21 @@ class ExceptionWithMessage(Exception):
 
 
 class EnvVarLoadingException(Exception):
+    """
+    Exception class for environment variable loading errors.
+
+    This exception can be raised when there is an issue related to loading environment variables.
+
+    Args:
+        *args: Additional arguments to pass to the base Exception class.
+
+    Example:
+        try:
+            settings = load_settings()
+        except Exception as e:
+            raise EnvVarLoadingException(e) from e
+
+        Output: EnvVarLoadingException: Failed to load environment variable
+    """
     def __init__(self, *args: object) -> None:
         super().__init__(*args)
