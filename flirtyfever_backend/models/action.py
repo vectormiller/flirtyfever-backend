f1a16462272ea8a551d912a926d6b1d4371bae89
Sxnchxs (Alex) <dxzim@bk.ru>
2023-09-21 20:33:03 +0600

    add: type reference

--- flirtyfever_backend/models/action.py
+++ flirtyfever_backend/models/action.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, Boolean, TIMESTAMP, ForeignKey
+from sqlalchemy import Column, Integer, TIMESTAMP, ForeignKey
 from sqlalchemy.sql import func
 from uuid import uuid4
 from ..database import Base
@@ -7,7 +7,7 @@ class Action(Base):
     __tablename__ = 'actions'
 
     id = Column(Integer, primary_key=True, index=True, unique=True, autoincrement=True)
-    from_id = Column(uuid4, ForeignKey("user.id"), nullable=False)
-    to_id = Column(uuid4, ForeignKey("user.id"), nullable=False)
-    type = Column(Boolean)
+    from_id = Column(uuid4, ForeignKey("users.id"), nullable=False)
+    to_id = Column(uuid4, ForeignKey("users.id"), nullable=False)
+    type = Column(Integer, ForeignKey("action_types.id"), nullable=False)
     performed_at = Column(TIMESTAMP, server_default=func.now())
