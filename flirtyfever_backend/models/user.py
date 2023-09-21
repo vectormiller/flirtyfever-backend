4b8627e93cd1e01f1b7d46fcef9a0334dfde8a9f
Sxnchxs (Alex) <dxzim@bk.ru>
2023-09-21 20:37:41 +0600

    add: unique number

--- flirtyfever_backend/models/user.py
+++ flirtyfever_backend/models/user.py
@@ -10,7 +10,7 @@ class User(Base):
     description = Column(String(255))
     role_id = Column(Integer, ForeignKey("roles.id"), nullable=False)
     created_at = Column(TIMESTAMP, server_default=func.now())
-    phone_number = Column(Integer, nullable=False)
+    phone_number = Column(Integer, unique=True, nullable=False)
     gender_id = Column(Integer, ForeignKey("genders.id"), nullable=False)
     city_id = Column(Integer, ForeignKey("cities.id"), nullable=False)
     rating = Column(Integer, server_default=30)
