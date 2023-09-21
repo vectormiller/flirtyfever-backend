7d8a96800d8821b7296e795b8fae4c8412a91a41
Sxnchxs (Alex) <dxzim@bk.ru>
2023-09-21 20:21:33 +0600

    add: nullable property

--- flirtyfever_backend/models/action.py
+++ flirtyfever_backend/models/action.py
@@ -7,7 +7,7 @@ class Action(Base):
     __tablename__ = 'actions'
 
     id = Column(Integer, primary_key=True, index=True, unique=True, autoincrement=True)
-    from_id = Column(uuid4, ForeignKey("user.id"))
-    to_id = Column(uuid4, ForeignKey("user.id"))
+    from_id = Column(uuid4, ForeignKey("user.id"), nullable=False)
+    to_id = Column(uuid4, ForeignKey("user.id"), nullable=False)
     type = Column(Boolean)
     performed_at = Column(TIMESTAMP, server_default=func.now())
